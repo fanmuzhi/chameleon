87f92b3b7cebc4e9cae466354d09ebe802405135
Qiling.Bo <Qiling.Bo@cn.synaptic.com>
2017-04-12 17:23:05 +0800

    update to latest

--- submodule.sh
+++ submodule.sh
@@ -1,6 +1,7 @@
-#git submodule add ssh://gitms@git.synaptics.com/git/biometrics/mt/cmlFpSensorEngine.git
+git submodule add ssh://gitms@git.synaptics.com/git/biometrics/mt/cmlFpSensorEngine.git
 git submodule add ssh://gitms@git.synaptics.com/git/biometrics/mt/cmlFpTestEngine.git
-#git submodule add ssh://gitms@git.synaptics.com/git/biometrics/mt/cmlFpTestApp.git
+git submodule add ssh://gitms@git.synaptics.com/git/biometrics/mt/cmlFpTestApp.git
 
 git submodule init
+git submodule update --init --recursive
 git status
