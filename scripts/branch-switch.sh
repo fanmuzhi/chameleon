c3e2356ba802f3b9d46ebb762ec079dd552cf089
Qiling.Bo <Qiling.Bo@cn.synaptic.com>
2017-12-21 14:20:31 +0800

    add scripts

--- scripts/branch-switch.sh
+++ scripts/branch-switch.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 #
-# Usage: ./scripts/branch-checkout.sh <branch_name>
+# Usage: ./scripts/branch-switch.sh <branch_name>
 #
 
 if [ "$1" == "" ]; then
-    echo "Switch to an existing branch"
+    echo "Switch to an existing branch without update"
     echo "USAGE  : $0 <branch_name>"
     echo "example: $0 develop"
 	exit
 fi
 
-# make sure current working directory is niteCore
+# make sure current working directory is niseCore
 cwd=$(basename $(pwd))
+echo "Current working directory: $cwd"
 
 if [ "$cwd" != "chameleon" ]; then
     echo "Please run the scrip from chameleon directory"
@@ -22,5 +23,4 @@ fi
 
 echo "Switching to branch: $1"
 git checkout $1
-git pull
-git submodule foreach --recursive 'branch="$(git -C $toplevel symbolic-ref --short HEAD)"; git checkout $branch; git pull;'
+git submodule foreach --recursive 'branch="$(git -C $toplevel symbolic-ref --short HEAD)"; git checkout $branch; : ;'
