4043f8d7206f0ea78e41ce708d32da02a7be5f10
Qiling.Bo <Qiling.Bo@cn.synaptic.com>
2018-02-24 16:12:35 +0800

    continue cleanup

--- submodule_clone.sh
+++
@@ -1,8 +0,0 @@
-# initialize the submodules, will pull the code first time
-git submodule init
-
-# update the submodule to current status
-git submodule update --init --recursive
-
-# display status
-git status
