6d63a4cc913519254f5b2e487323f59e85efda27
Vincent.W.Chen <vincent.w.chen@synaptics.com>
2018-04-08 15:35:43 +0800

    Fixed x64 building issue, divided build bat files into release and debug

--- cleanAll.bat
+++ cleanAll.bat
@@ -1,17 +1,4 @@
-REM cd chameeleon
 call cmakescripts\common\buildscripts\buildvars.bat c:\tools
 make -f cmakescripts\common\buildscripts\Makefile clean
-REM cd ..
-
-REM cd niteCore
-REM call common\buildscripts\buildvars.bat c:\tools
-REM make -f common\buildscripts\Makefile clean
-REM cd ..
 
 pause
-
-REM echo .
-REM echo Removing prebuilt folder ...
-REM rmdir /s /q prebuilt
-REM 
-REM pause
